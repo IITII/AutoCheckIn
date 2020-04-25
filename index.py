2d1d26146f0acb658b502bdd32127a7f83234197
IITII <ccmejx@gmail.com>
2020-04-25 23:57:36 +0800

    update: 1. Update icon 2. Add bat script

--- index.py
+++ index.py
@@ -8,9 +8,36 @@ def toast(title, info):
     if title is not None:
         toaster.show_toast(title,
                            info,
-                           icon_path="icon.png",
-                           duration=10,
+                           icon_path="icon.ico",
+                           duration=3,
                            threaded=True)
+
+
+def task(code):
+    if code is None:
+        print('code could not be None...')
+        return
+    if code == 0:
+        info = {
+            "filename": "./checkIn.js",
+            "tip": "自动签到",
+            "msg": "msg"
+        }
+    elif code == 1:
+        info = {
+            "filename": "./index.js",
+            "tip": "调查问卷",
+            "msg": "msg1"
+        }
+    else:
+        print('No such code')
+        return
+    driver.execute_script(open(info.get("filename"), encoding='utf-8').read())
+    time.sleep(2)
+    msg = driver.find_element_by_class_name('foot').get_attribute(info.get("msg"))
+    print(msg)
+    msg = json.loads(msg)
+    toast(info.get("tip"), msg.get('msg'))
 
 
 toaster = ToastNotifier()
@@ -18,28 +45,17 @@ driver = webdriver.Chrome()
 loginInfo = {
     "loginName": "学号",
     "yzxx": "姓名",
-    "loginType": 0 #身份，默认为0，其他请详查网页代码
+    "loginType": 0  # 身份，默认为0，其他请详查网页代码
 }
 # try:
 driver.get("https://fxgl.jx.edu.cn/4136010406/public/homeQd?loginName="
            + loginInfo.__getitem__('loginName')
            + "&loginType=" + str(loginInfo.__getitem__('loginType')))
 time.sleep(1)
-print("自动签到...")
-driver.execute_script(open('./checkIn.js', encoding='utf-8').read())
-time.sleep(2)
-msg = driver.find_element_by_class_name('foot').get_attribute('msg')
-print(msg)
-msg = json.loads(msg)
-toast("自动签到", msg['msg'])
-
-print("调查问卷...")
-driver.execute_script(open("./index.js", encoding='utf-8').read())
-time.sleep(2)
-msg1 = driver.find_element_by_class_name('foot').get_attribute('msg1')
-msg1 = json.loads(msg1)
-print(msg1)
-toast("调查问卷", msg1['msg'])
-print("10s 后自动退出...")
+task(0)
+time.sleep(3)
+task(1)
+print("10s 后关闭浏览器...")
 time.sleep(10)
+print("浏览器已关闭")
 driver.quit()
